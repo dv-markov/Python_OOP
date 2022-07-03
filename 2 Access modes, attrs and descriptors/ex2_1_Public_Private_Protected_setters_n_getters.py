c89b6288cc1215eef1a6f96bea4ee552ca43937a
dv-markov <dmitry.v.markov@gmail.com>
2022-07-03 23:48:32 +0300

    HOME-COMMIT

--- 2 Access modes, attrs and descriptors/ex2_1_Public_Private_Protected_setters_n_getters.py
+++ 2 Access modes, attrs and descriptors/ex2_1_Public_Private_Protected_setters_n_getters.py
@@ -240,3 +240,118 @@ m1 = mn_1.get_money()    # 100
 m2 = mn_2.get_money()    # 120
 
 print(m1, m2)
+
+
+# Task 5
+class Book:
+    def __init__(self, auth, title, price):
+        self.__title = title
+        self.__author = auth
+        self.__price = price
+
+    def set_title(self, title):
+        self.__title = title
+
+    def set_author(self, author):
+        self.__author = author
+
+    def set_price(self, price):
+        self.__price = price
+
+    def get_title(self):
+        return self.__title
+
+    def get_author(self):
+        return self.__author
+
+    def get_price(self):
+        return self.__price
+
+
+book = Book('автор', 'название', 'цена')
+print(book.get_title(), book.get_author(), book.get_price())
+
+
+# Task 7
+class Line:
+    def __init__(self, x1, y1, x2, y2):
+        self.__x1 = x1
+        self.__y1 = y1
+        self.__x2 = x2
+        self.__y2 = y2
+
+    def set_coords(self, x1, y1, x2, y2):
+        self.__x1 = x1
+        self.__y1 = y1
+        self.__x2 = x2
+        self.__y2 = y2
+
+    def get_coords(self):
+        return self.__x1, self.__y1, self.__x2, self.__y2
+
+    def draw(self):
+        print(self.__x1, self.__y1, self.__x2, self.__y2)
+
+
+line = Line(1, 2, 3, 4)
+print(line.get_coords())
+line.set_coords(5, 6, 7, 8)
+line.draw()
+
+
+# Task 8
+class Point:
+    def __init__(self, x, y):
+        if self.check_coord(x) and self.check_coord(y):
+            self.__x = x
+            self.__y = y
+        else:
+            raise ValueError("Координаты точки должны быть числами")
+
+    @staticmethod
+    def check_coord(x):
+        return type(x) in (int, float)
+
+    def get_coords(self):
+        return self.__x, self.__y
+
+
+class Rectangle:
+    def __init__(self, *args):
+        if len(args) == 2 and self.check_points(*args):
+            self.__sp = args[0]
+            self.__ep = args[1]
+        elif len(args) == 4:
+            self.__sp = Point(*args[:2])
+            self.__ep = Point(*args[2:])
+        else:
+            raise ValueError("Координаты прямоугольника должны быть точками или числами")
+
+    @staticmethod
+    def check_points(*args):
+        return all(map(lambda x: isinstance(x, Point), args))
+
+    def set_coords(self, sp, ep):
+        if self.check_points(sp, ep):
+            self.__sp = sp
+            self.__ep = ep
+
+    def get_coords(self):
+        return self.__sp, self.__ep
+
+    def draw(self):
+        print(f"Прямоугольник с координатами: {self.__sp.get_coords()} {self.__ep.get_coords()}")
+
+pt = Point(100, 200)
+print(pt.__dict__)
+print(pt.get_coords())
+
+rect = Rectangle(0, 0, 20, 34)
+print(rect.get_coords())
+rect.draw()
+
+rect2 = Rectangle(Point(100, 100), Point(200, 300))
+rect2.set_coords(Point(500, 500), Point(700, 700))
+print(rect2.get_coords())
+rect2.draw()
+
