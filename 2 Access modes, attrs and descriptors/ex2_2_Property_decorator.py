8eb196ca3e6fd2f38473f54fb446d71b31483e0b
dv-markov <dmitry.v.markov@gmail.com>
2022-07-09 11:49:35 +0300

    HOME-COMMIT

--- 2 Access modes, attrs and descriptors/ex2_2_Property_decorator.py
+++ 2 Access modes, attrs and descriptors/ex2_2_Property_decorator.py
@@ -401,3 +401,41 @@ print(dist)
 p.add_line(LineTo(20, -10))
 dist = p.get_length()
 print(dist)
+
+
+# Variant 2
+class LineTo:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def length(self, x1, y2):
+        return ((self.x - x1) ** 2 + (self.y - y2) ** 2) ** 0.5
+
+
+class PathLines:
+    def __init__(self, *points):
+        self.__points = list(points)
+
+    def add_line(self, point):
+        self.__points.append(point)
+
+    def get_path(self):
+        return self.__points
+
+    def get_length(self):
+        x = y = length = 0
+        for point in self.__points:
+            length += point.length(x, y)
+            x, y = point.x, point.y
+        return length
+
+
+p0 = PathLines(1, 2, 3)
+print(p0.get_path())
+p = PathLines(LineTo(10, 20), LineTo(10, 30))
+dist = p.get_length()
+print(dist)
+p.add_line(LineTo(20, -10))
+dist = p.get_length()
+print(dist)
