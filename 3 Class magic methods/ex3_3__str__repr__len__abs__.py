bb6cb772cd01cc18960f4e65130bb38284b2194f
dv-markov <dmitry.v.markov@gmail.com>
2022-07-26 00:15:31 +0300

    HOME-COMMIT

--- 3 Class magic methods/ex3_3__str__repr__len__abs__.py
+++ 3 Class magic methods/ex3_3__str__repr__len__abs__.py
@@ -240,3 +240,43 @@ print(l1(2))
 #         if indx != math.inf and i != indx:
 #             raise AttributeError('Index out of range')
 #     return i, obj
+
+
+# Task 6
+class Complex:
+    def __init__(self, real, img):
+        self.real = real
+        self.img = img
+
+    @property
+    def real(self):
+        return self.__real
+
+    @real.setter
+    def real(self, value):
+        self.__real = value
+
+    @property
+    def img(self):
+        return self.__img
+
+    @img.setter
+    def img(self, value):
+        self.__img = value
+
+    def __setattr__(self, key, value):
+        if type(value) in (int, float):
+            super().__setattr__(key, value)
+        else:
+            raise ValueError("Неверный тип данных.")
+
+    def __abs__(self):
+        return (self.real**2 + self.img**2)**0.5
+
+
+cmp = Complex(7, 8)
+cmp.real = 3
+cmp.img = 4
+c_abs = abs(cmp)
+
+print(c_abs)
