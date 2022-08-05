27356094c35342fa1dd7698aa52956b40e0dceed
dv-markov <dmitry.v.markov@gmail.com>
2022-08-05 23:34:11 +0300

    WORK-COMMIT

--- 3 Class magic methods/ex3_5___eq__ne__lt__gt__.py
+++ 3 Class magic methods/ex3_5___eq__ne__lt__gt__.py
@@ -432,3 +432,93 @@ class FileAcceptor:
             return FileAcceptor(*self.extensions.union(other.extensions))
 
 
+# Task 8
+class Money:
+    def __init__(self, volume=0):
+        self.cb = None
+        self.volume = volume
+
+    @property
+    def cb(self):
+        return self.__cb
+
+    @cb.setter
+    def cb(self, value):
+        self.__cb = value
+
+    @property
+    def volume(self):
+        return self.__volume
+
+    @volume.setter
+    def volume(self, value):
+        self.__volume = value
+
+    def convert_to_rub(self):
+        cr = self.cb.rates
+        return round(self.volume / cr[str(self)] * cr['rub'], 2)
+
+    def __check_registration(self, other):
+        if not (self.cb or other.cb):
+            raise ValueError("Неизвестен курс валют.")
+
+    def __eq__(self, other):
+        self.__check_registration(other)
+        if isinstance(other, Money):
+            return self.convert_to_rub() == other.convert_to_rub()
+
+    def __gt__(self, other):
+        self.__check_registration(other)
+        if isinstance(other, Money):
+            return self.convert_to_rub() > other.convert_to_rub()
+
+    def __ge__(self, other):
+        self.__check_registration(other)
+        if isinstance(other, Money):
+            return self.convert_to_rub() >= other.convert_to_rub()
+
+
+class MoneyR(Money):
+    def __repr__(self):
+        return 'rub'
+
+
+class MoneyD(Money):
+    def __repr__(self):
+        return 'dollar'
+
+
+class MoneyE(Money):
+    def __repr__(self):
+        return 'euro'
+
+
+class CentralBank:
+    rates = {'rub': 72.5, 'dollar': 1.0, 'euro': 1.15}
+
+    def __new__(cls, *args, **kwargs):
+        return
+
+    @classmethod
+    def register(cls, money):
+        money.cb = cls
+
+
+rub = MoneyR(1500)
+CentralBank.register(rub)
+usd = MoneyD(700)
+CentralBank.register(usd)
+
+print(rub == usd)
+print(rub < usd)
+print(rub >= usd)
+
+CentralBank.rates = {'rub': 72.5, 'dollar': 1.0, 'euro': 1.15}
+r = MoneyR(45000)
+d = MoneyD(500)
+CentralBank.register(r)
+CentralBank.register(d)
+if r > d:
+    print("неплохо")
+else:
+    print("нужно поднажать")
