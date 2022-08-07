330dc9be963f44b06464b4d56bc59c5696b67b75
dv-markov <dmitry.v.markov@gmail.com>
2022-08-07 16:30:47 +0300

    WORK-COMMIT

--- 3 Class magic methods/ex3_5___eq__ne__lt__gt__.py
+++ 3 Class magic methods/ex3_5___eq__ne__lt__gt__.py
@@ -522,3 +522,147 @@ if r > d:
     print("неплохо")
 else:
     print("нужно поднажать")
+
+
+# Task 9
+class Body:
+    def __init__(self, name, ro, volume):
+        self.name = name
+        self.ro = ro
+        self.volume = volume
+
+    def get_mass(self, other):
+        s_mass = self.ro * self.volume
+        if isinstance(other, Body):
+            return s_mass, other.ro * other.volume
+        elif type(other) in (int, float):
+            return s_mass, other
+        else:
+            raise TypeError('Второй операнд должен быть типа Body или числом')
+
+    def __eq__(self, other):
+        a, b = self.get_mass(other)
+        return a == b
+
+    def __lt__(self, other):
+        a, b = self.get_mass(other)
+        return a < b
+
+
+body1 = Body('body1', 100, 500)
+body2 = Body('body2', 500, 10)
+
+print(body1 > body2)  # True, если масса тела body1 больше массы тела body2
+print(body1 == body2) # True, если масса тела body1 равна массе тела body2
+print(body1 < 10)     # True, если масса тела body1 меньше 10
+print(body2 == 5)     # True, если масса тела body2 равна 5
+
+# Variant 2
+# def mass_arg(func):
+#     def wrapper(instance, other, *args):
+#         if isinstance(other, Body):
+#             return func(instance, other.mass)
+#         elif isinstance(other, (int, float)):
+#             return func(instance, other)
+#         else:
+#             raise TypeError(f"Not supported type {type(other)} in {func}")
+#
+#     return wrapper
+#
+#
+# class Body:
+#     def __init__(self, name, ro, volume):
+#         self.name = name
+#         self.ro = ro
+#         self.volume = volume
+#
+#     @property
+#     def mass(self):
+#         return (self.ro * self.volume)
+#
+#     @mass_arg
+#     def __lt__(self, other):
+#         return (self.mass < other)
+#
+#     @mass_arg
+#     def __le__(self, other):
+#         return self.mass <= other
+#
+#     @mass_arg
+#     def __eq__(self, other):
+#         return self.mass == other
+
+# Variant 3
+# class Body:
+#     def __init__(self, name, ro, volume):
+#         if type(name) == str:
+#             self.name = name
+#         if isinstance(ro, (int, float)):
+#             self.ro = ro
+#         if isinstance(volume, (int, float)):
+#             self.volume = volume
+#
+#     def weight(self):
+#         return self.ro * self.volume
+#
+#     @staticmethod
+#     def choose(other):
+#         if isinstance(other, Body):
+#             return other.weight()
+#         elif isinstance(other, (int, float)):
+#             return other
+#         raise ValueError("Неведома зверюшка")
+#
+#     def __eq__(self, other):
+#         return self.weight() == self.choose(other)
+#
+#     def __lt__(self, other):
+#         return self.weight() < self.choose(other)
+
+
+# Task 10
+class Box:
+    def __init__(self):
+        self.things = []
+
+    def add_thing(self, obj):
+        if isinstance(obj, Thing):
+            self.things.append(obj)
+
+    def get_things(self):
+        return self.things
+
+    def __eq__(self, other):
+        if isinstance(other, Box):
+            st = self.things
+            ot = other.things
+            return len(st) == len(ot) and all(st.count(x) == 1 for x in ot)
+
+
+class Thing:
+    def __init__(self, name, mass):
+        self.name = name
+        self.mass = mass
+
+    def __eq__(self, other):
+        if isinstance(other, Thing):
+            return self.name.lower() == other.name.lower() and self.mass == other.mass
+
+
+print('Task 10:')
+t1 = t2 = Thing('мел', 100)
+print(t1 == t2)
+
+b1 = Box()
+b2 = Box()
+
+b1.add_thing(Thing('мел', 100))
+b1.add_thing(Thing('тряпка', 200))
+b1.add_thing(Thing('доска', 2000))
+
+b2.add_thing(Thing('тряпка', 200))
+b2.add_thing(Thing('мел', 100))
+b2.add_thing(Thing('доска', 2000))
+
+res = b1 == b2  # True
+print(res)
