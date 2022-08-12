6e607453b6306d553ae4fd2cb4cdf4d25694307b
dv-markov <dmitry.v.markov@gmail.com>
2022-08-12 19:43:07 +0300

    WORK-COMMIT

--- 3 Class magic methods/ex3_7__bool__.py
+++ 3 Class magic methods/ex3_7__bool__.py
@@ -154,3 +154,39 @@ for p in players_filtered:
 
 
 # Task 5
+import sys
+
+
+class MailBox:
+    def __init__(self):
+        self.inbox_list = []
+
+    def receive(self):
+        # lst_in = list(map(str.strip, sys.stdin.readlines()))
+        lst_in = ['sc_lib@list.ru; От Балакирева; Успехов в IT!',
+                  'mail@list.ru; Выгодное предложение; Вам одобрен кредит.',
+                  'mail123@list.ru; Розыгрыш; Вы выиграли 1 млн. руб. Переведите 30 тыс. руб., чтобы его получить.']
+        self.inbox_list += [MailItem(*msg.split('; ')) for msg in lst_in]
+
+class MailItem:
+    def __init__(self, mail_from, title, content):
+        self.mail_from = mail_from
+        self.title = title
+        self.content = content
+        self.is_read = False
+
+    def set_read(self, fl_read=True):
+        self.is_read = fl_read
+
+    def __bool__(self):
+        return self.is_read
+
+
+mail = MailBox()
+mail.receive()
+mail.inbox_list[0].set_read()
+mail.inbox_list[-1].set_read()
+inbox_list_filtered = list(filter(bool, mail.inbox_list))
+
+for msg in inbox_list_filtered:
+    print(msg.__dict__)
