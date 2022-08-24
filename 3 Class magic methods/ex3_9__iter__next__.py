4becb4ddbcb39bd633ca25da275f3ea9cb0a98b3
dv-markov <dmitry.v.markov@gmail.com>
2022-08-24 23:59:18 +0300

    HOME-COMMIT

--- 3 Class magic methods/ex3_9__iter__next__.py
+++ 3 Class magic methods/ex3_9__iter__next__.py
@@ -1,5 +1,7 @@
 # 3.9 Методы __iter__ и __next__
 
+# __iter__(self) - получение итератора для перебора объекта
+# __next__(self) - переход к следующему значению и его считывание
 
 lst = [8, 5, 3, 1, 7]
 it = iter(lst)
@@ -40,8 +42,8 @@ print(next(fr))
 # Stop iteration
 # print(fr.__next__())
 
-# Итератор - объект, для которого определена функция __next__.
-# Итерируемый объект - объект, для которого определена функция __iter__.
+# Итератор - объект, для которого определен магический метод __next__.
+# Итерируемый объект - объект, для которого определен магический метод __iter__.
 
 # По умолчанию объекты (даже итераторы) не являются итерируемыми объектами
 # fr = FRange(0, 2, 0.5)
@@ -55,6 +57,10 @@ class FRange:
         self.stop = stop
         self.step = step
 
+    def __iter__(self):
+        self.value = self.start - self.step
+        return self
+
     def __next__(self):
         if self.value + self.step < self.stop:
             self.value += self.step
@@ -62,14 +68,13 @@ class FRange:
         else:
             raise StopIteration
 
-    def __iter__(self):
-        self.value = self.start - self.step
-        return self
-
 
 print('\n', 'Итерируемый объект:', sep='')
 fr = FRange(0, 2, 0.5)
-for x in fr: # не сработает, если функция __iter__ не определена
+# цикл работает, потому что определен метод __iter__ и объект является итерируемым
+# функция for сначала получает итератор (неявно вызывает метод __iter__),
+# а потом с помощью неявного вызова метода __next__ последовательно перебирает итерируемый объект
+for x in fr:
     print(x)
 
 
@@ -85,7 +90,8 @@ class FRange2D:
     def __next__(self):
         if self.value < self.rows:
             self.value += 1
-            return iter(self.fr)
+            # return iter(self.fr)
+            return self.fr
         else:
             raise StopIteration
 
@@ -98,4 +104,47 @@ for row in fr:
     print()
 
 
+# Task 5
+class Person:
+    def __init__(self, fio, job, old, salary, year_job):
+        self.fio = fio
+        self.job = job
+        self.old = old
+        self.salary = salary
+        self.year_job = year_job
 
+    @staticmethod
+    def __check(indx):
+        if not isinstance(indx, int) or not (0 <= indx <= 4):
+            raise IndexError('неверный индекс')
+
+    def __getitem__(self, item):
+        self.__check(item)
+        return list(self.__dict__.values())[item]
+
+    def __setitem__(self, key, value):
+        self.__check(key)
+        setattr(self, list(self.__dict__.keys())[key], value)
+
+    def __iter__(self):
+        self.values = list(self.__dict__.values())
+        self.ln = len(self.values)
+        self.i = -1
+        return self
+
+    def __next__(self):
+        if self.i < self.ln - 1:
+            self.i += 1
+            return self.values[self.i]
+        else:
+            raise StopIteration
+
+
+pers = Person('Гейтс Б.', 'бизнесмен', 61, 1000000, 46)
+print(pers.__dict__)
+
+for x in pers:
+    print(x)
+pers[0] = 'Dmitry Markov'
+print(pers[0])
+print(pers[1])
