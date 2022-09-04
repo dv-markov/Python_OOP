24e98e123d58601d8433e468933dd9e5b0781cc8
dv-markov <dmitry.v.markov@gmail.com>
2022-09-04 23:01:28 +0300

    HOME-COMMIT

--- 3 Class magic methods/ex3_9__iter__next__.py
+++ 3 Class magic methods/ex3_9__iter__next__.py
@@ -250,3 +250,31 @@ it_iter = iter(it1)
 x = next(it_iter)
 print(x)
 
+
+# Task 7
+class IterColumn:
+    def __init__(self, lst, column):
+        self._lst = lst
+        self._col = column
+
+    def __iter__(self):
+        for i in range(len(self._lst)):
+            yield self._lst[i][self._col]
+
+
+lst = [[11, 12, 13, 14], [21, 22, 23, 24], [31, 32, 33, 34], [41, 42, 43, 44]]
+
+it = IterColumn(lst, 1)
+for x in it:  # последовательный перебор всех элементов столбца списка: x12, x22, ..., xM2
+    print(x)
+
+it_iter = iter(it)
+x = next(it_iter)
+print(x)
+
+# # Variant 2 - Balakirev
+# def __iter__(self):
+#     for row in self._lst:
+#         yield row[self._col]
+
+
