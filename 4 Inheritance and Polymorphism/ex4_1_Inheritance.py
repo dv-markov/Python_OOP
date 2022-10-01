69a03ec83b5a94d69388be824d6d9fab46fa3ba1
dv-markov <dmitry.v.markov@gmail.com>
2022-10-01 13:20:11 +0300

    HOME-COMMIT

--- 4 Inheritance and Polymorphism/ex4_1_Inheritance.py
+++ 4 Inheritance and Polymorphism/ex4_1_Inheritance.py
@@ -77,3 +77,51 @@ cat = Cat('кот', 4, 'black', 2.25)
 dog = Dog('Шарик', 7, 'Zwergpinscher', 8)
 print(cat.get_info())
 print(dog.get_info())
+
+
+# Task 5
+class Thing:
+    __id = -1
+
+    def __init__(self, name, price):
+        self.id = self.__get_id()
+        self.name = name
+        self.price = price
+        self.weight = None
+        self.dims = None
+        self.memory = None
+        self.frm = None
+
+    @classmethod
+    def __get_id(cls):
+        Thing.__id += 1
+        return cls.__id
+
+    def get_data(self):
+        return tuple(self.__dict__.values())
+
+
+class Table(Thing):
+    def __init__(self, name, price, weight, dims):
+        super().__init__(name, price)
+        self.weight = weight
+        self.dims = dims
+
+
+class ElBook(Thing):
+    def __init__(self, name, price, memory, frm):
+        super().__init__(name, price)
+        self.memory = memory
+        self.frm = frm
+
+
+a = Thing(1, 2000)
+b = Thing(2, 3000)
+print(a.__dict__)
+print(b.__dict__)
+print(a.get_data())
+
+table = Table("Круглый", 1024, 812.55, (700, 750, 700))
+book = ElBook("Python ООП", 2000, 2048, 'pdf')
+print(*table.get_data())
+print(*book.get_data())
