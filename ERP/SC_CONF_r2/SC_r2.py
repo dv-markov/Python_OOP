4b350d94f2b09dcc29b4b3ad48eaf4cff7e15576
dv-markov <dmitry.v.markov@gmail.com>
2022-10-09 01:51:55 +0300

    WORK-COMMIT

--- ERP/SC_CONF_r2/SC_r2.py
+++ ERP/SC_CONF_r2/SC_r2.py
@@ -60,58 +60,90 @@ class Seat(Part):
                       'mat': self.get_list(mat)}
 
 
-def invite_message():
-    def invite():
-        print()
-        print("Выберите действие и введите соответствующую цифру:", "\n",
-              "1 - Задать конфигурацию клапана, 2 - Отобразить доступные детали, 0 - Выход")
-
-        return input("> ")
-
-    s = invite()
-    while s not in ('1', '2', '0'):
-        print('Ошибка ввода!')
-        s = invite()
-
-    return s
-
-
-def dashes(n):
-    return '-' * n
-
-
 if __name__ == '__main__':
-    title = "Конфигуратор Самсон Контролс, версия 0.2а"
-    print(dashes(len(title)), title, dashes(len(title)), sep='\n')
     # текстовые переменные
     part_names1 = {'Body': 'КОРПУС', 'Bonnet': 'КРЫШКА', 'Seat': 'СЕДЛО'}
     part_names2 = {'Body': 'корпуса', 'Bonnet': 'крышки', 'Seat': 'седла'}
+
+    def invite_message():
+        def invite():
+            print()
+            print("Введите одну из указанных ниже цифр для выполнения соответствующего действия:", "\n",
+                  "1 - Задать конфигурацию клапана, 2 - Отобразить доступные параметры клапана, "
+                  "3 - Отобразить доступные детали, 0 - Выход")
+            return input("> ")
+        s = invite()
+        while s not in ('1', '2', '3', '0'):
+            print('Ошибка ввода!')
+            s = invite()
+        return s
+
+    def dashes(n):
+        return '-' * n
+
+    def get_value(x):
+        val = input(f'Введите {x}: ')
+        while True:
+            try:
+                val = int(val) if x in ('DN', 'PN', 'Kvs') else val.upper()
+            except ValueError:
+                print(f"Ошибка ввода! {x} должен быть числом")
+                val = input(f'Введите {x}: ')
+            else:
+                break
+        return val
+
+    title = "Конфигуратор Самсон Контролс, версия 0.2а"
+    print(dashes(len(title)), title, dashes(len(title)), sep='\n')
+
     # заполнение склада
     inventory = fill_inventory()
+    # создание наборов параметров:
+    avl_params = {key: set() for key in ('тип клапана', 'DN', 'PN', 'материал корпуса', 'Kvs')}
+    atr_names = {'dn': 'DN', 'pn': 'PN', 'mat': 'материал корпуса', 'kvs': 'Kvs'}
+    for part in inventory:
+        for attr, values in part.attrs.items():
+            avl_params[atr_names[attr]].update(values)
+    avl_params['тип клапана'] = {'3241', }
+    avl_params['PN'] = {25, 40}
 
+    # начало работы с пользователем
     s1 = invite_message()
     while s1 != '0':
         print()
 
-        if s1 == '2':
+        if s1 == '3':
             print('Доступные детали:')
             print(dashes(50))
             for item in inventory:
-                print(item.__class__.__name__, item.__dict__)
+                print(item, *(f'{atr_names[key]}: {value}' for key, value in item.attrs.items()), sep='; ')
+            print(dashes(50))
+
+        elif s1 == '2':
+            print('Доступные параметры клапана:')
+            print(dashes(50))
+            for pr in avl_params:
+                print(f' {pr}: {sorted(avl_params[pr])}')
             print(dashes(50))
 
         elif s1 == '1':
-            valve_param = dict.fromkeys(('тип клапана', 'DN', 'PN', 'материал корпуса', 'Kvs'), None)
+            valve_param = dict.fromkeys((avl_params.keys()), None)
             for x in valve_param:
-                value = input(f'Введите {x}: ')
-                valve_param[x] = int(value) if x in ('DN', 'PN', 'Kvs') else value.upper()
+                value = get_value(x)
+                while not {value, }.issubset(avl_params[x]):
+                    print(f'Ошибка ввода! Данный {x} недоступен для внесенного перечня деталей')
+                    print(f"Доступные значения для параметра '{x}': ", end='')
+                    print(*sorted(avl_params[x]), sep=', ')
+                    value = get_value(x)
+                valve_param[x] = value
+
             print('\n', 'Введены параметры клапана:', sep='')
-            print(valve_param)
+            print(valve_param, '\n')
             # valve_param = {'тип клапана': 3241, 'DN': 50, 'PN': 40, 'материал корпуса': '20ГЛ', 'Kvs': 25}
 
             v1 = Type3241(*valve_param.values())
-            print('\n', 'Создан объект по шаблону "клапан 3241":', sep='')
-            print(v1.__dict__, '\n')
+            # print('\n', f'Создан объект по шаблону "клапан {v1.valve_type}":', sep='')
+            # print(v1.__dict__, '\n')
 
             input('...Нажмите клавишу ВВОД для создания BOM...')
             v1.get_bom()
