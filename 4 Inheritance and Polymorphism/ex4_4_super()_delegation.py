f551c6bfa8a5b90e88bf7329438667f6b40800fe
dv-markov <dmitry.v.markov@gmail.com>
2022-10-24 00:28:38 +0300

    WORK-COMMIT

--- 4 Inheritance and Polymorphism/ex4_4_super()_delegation.py
+++ 4 Inheritance and Polymorphism/ex4_4_super()_delegation.py
@@ -240,4 +240,36 @@ class Land(SellItem):
 
 
 class Agency:
-    pass
+    def __init__(self, name):
+        self.name = name
+        self.obj_list = []
+
+    def add_object(self, obj):
+        if isinstance(obj, SellItem):
+            self.obj_list.append(obj)
+
+    def remove_object(self, obj):
+        if obj in self.obj_list:
+            self.obj_list.remove(obj)
+
+    def get_objects(self):
+        return self.obj_list
+
+
+ag = Agency("Рога и копыта")
+flat1 = Flat("квартира, 3к", 10000000, 121.5, 3)
+ag.add_object(flat1)
+ag.add_object(Flat("квартира, 2к", 8000000, 74.5, 2))
+ag.add_object(Flat("квартира, 1к", 4000000, 54, 1))
+ag.add_object(House("дом, кирпичный", price=35000000, material="кирпич", square=186.5))
+ag.add_object(Land("участок под застройку", 3000000, 6.74))
+ag.remove_object(flat1)
+
+for obj in ag.get_objects():
+    print(obj.name)
+
+lst_houses = [x for x in ag.get_objects() if isinstance(x, House)] # выделение списка домов
+print(lst_houses, *(x.name for x in lst_houses))
+
+
+# Task 6
