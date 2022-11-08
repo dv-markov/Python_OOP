84060e7cc48ebf2c60fc823b281367f1bbc4f55d
dv-markov <dmitry.v.markov@gmail.com>
2022-11-09 00:49:42 +0300

    WORK-COMMIT

--- 4 Inheritance and Polymorphism/ex4_4_super()_delegation.py
+++ 4 Inheritance and Polymorphism/ex4_4_super()_delegation.py
@@ -273,3 +273,33 @@ print(lst_houses, *(x.name for x in lst_houses))
 
 
 # Task 6
+class Router:
+    app = {}
+
+    @classmethod
+    def get(cls, path):
+        return cls.app.get(path)
+
+    @classmethod
+    def add_callback(cls, path, func):
+        cls.app[path] = func
+
+
+class Callback:
+    def __init__(self, path, route_cls):
+        self.path = path
+        self.route_cls = route_cls
+
+    def __call__(self, func):
+        self.route_cls.add_callback(self.path, func)
+
+
+@Callback('/', Router)
+def index():
+    return '<h1>Главная</h1>'
+
+
+route = Router.get('/')
+if route:
+    ret = route()
+    print(ret)
