e1ee13365a0bd5775599d2800754d18718c81ae4
dv-markov <dmitry.v.markov@gmail.com>
2022-11-23 00:31:04 +0300

    WORK-COMMIT

--- 4 Inheritance and Polymorphism/ex4_4_Private_protected.py
+++ 4 Inheritance and Polymorphism/ex4_4_Private_protected.py
@@ -228,7 +228,7 @@ class Furniture:
         # вариант с getattr, работает всегда
         if key in ('_name', '_weight'):
             print(f'_{__class__.__name__}__verify{key}', value)
-            # изучать до конца тему с аннотациями типов
+            # изучить до конца тему с аннотациями типов
             # print(inspect.get_annotations(self.__class__))
             # print(self.__annotations__)
             getattr(self, f'_{__class__.__name__}__verify{key}')(value)
@@ -282,3 +282,104 @@ print(Closet.__mro__)
 print(Closet.__base__)
 print(Closet.__bases__)
 print(Furniture.__subclasses__())
+
+
+# Task 6
+class Observer:
+    def update(self, data):
+        pass
+
+    def __hash__(self):
+        return hash(id(self))
+
+
+class Subject:
+    def __init__(self):
+        self.__observers = {}
+        self.__data = None
+
+    def add_observer(self, observer):
+        self.__observers[observer] = observer
+
+    def remove_observer(self, observer):
+        if observer in self.__observers:
+            self.__observers.pop(observer)
+
+    def __notify_observer(self):
+        for ob in self.__observers:
+            ob.update(self.__data)
+
+    def change_data(self, data):
+        self.__data = data
+        self.__notify_observer()
+
+
+class Data:
+    def __init__(self, temp, press, wet):
+        self.temp = temp    # температура
+        self.press = press  # давление
+        self.wet = wet      # влажность
+
+
+class TemperatureView(Observer):
+    def update(self, data):
+        print(f"Текущая температура {data.temp}")
+
+
+class PressureView(Observer):
+    def update(self, data):
+        print(f"Текущее давление {data.press}")
+
+
+class WetView(Observer):
+    def update(self, data):
+        print(f"Текущая влажность {data.wet}")
+
+
+subject = Subject()
+tv = TemperatureView()
+pr = PressureView()
+wet = WetView()
+
+subject.add_observer(tv)
+subject.add_observer(pr)
+subject.add_observer(wet)
+
+subject.change_data(Data(23, 150, 83))
+# выведет строчки:
+# Текущая температура 23
+# Текущее давление 150
+# Текущая влажность 83
+subject.remove_observer(wet)
+subject.change_data(Data(24, 148, 80))
+# выведет строчки:
+# Текущая температура 24
+# Текущее давление 148
+
+
+# Variant 2 - через BaseView
+# class BaseView(Observer):
+#     msg = None
+#     attr = None
+#
+#     def update(self, data):
+#         attr = getattr(data, self.attr)
+#         print(f'{self.msg} {attr}')
+#
+#
+# class TemperatureView(BaseView):
+#     msg = 'Текущая температура'
+#     attr = 'temp'
+#
+#
+# class PressureView(BaseView):
+#     msg = 'Текущее давление'
+#     attr = 'press'
+#
+#
+# class WetView (BaseView):
+#     msg = 'Текущая влажность'
+#     attr = 'wet'
+
+
+# Task 7
