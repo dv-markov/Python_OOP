fb65ea7b58903d88811a1dd7abc2fe11a713bc8a
dv-markov <dmitry.v.markov@gmail.com>
2022-11-26 10:03:46 +0300

    WORK-COMMIT

--- 4 Inheritance and Polymorphism/ex4_4_Private_protected.py
+++ 4 Inheritance and Polymorphism/ex4_4_Private_protected.py
@@ -533,3 +533,41 @@ print(Vector.__dict__)
 vec = Vector(1, 2, 3)
 vec[0] = 10
 print(vector_log)
+
+
+# Task 10
+CURRENT_OS = 'windows'   # 'windows', 'linux'
+
+
+class WindowsFileDialog:
+    def __init__(self, title, path, exts):
+        self.__title = title # заголовок диалогового окна
+        self.__path = path  # начальный каталог с файлами
+        self.__exts = exts  # кортеж из отображаемых расширений файлов
+
+
+class LinuxFileDialog:
+    def __init__(self, title, path, exts):
+        self.__title = title # заголовок диалогового окна
+        self.__path = path  # начальный каталог с файлами
+        self.__exts = exts  # кортеж из отображаемых расширений файлов
+
+
+class FileDialogFactory:
+    def __new__(cls, *args, **kwargs):
+        if CURRENT_OS == 'windows':
+            return cls.create_windows_filedialog(*args, **kwargs)
+        return cls.create_linux_filedialog(*args, **kwargs)
+
+    @staticmethod
+    def create_windows_filedialog(*args, **kwargs):
+        return WindowsFileDialog(*args, **kwargs)
+
+    @staticmethod
+    def create_linux_filedialog(*args, **kwargs):
+        return LinuxFileDialog(*args, **kwargs)
+
+
+dlg = FileDialogFactory('Изображения', 'd:/images/', ('jpg', 'gif', 'bmp', 'png'))
+print(dlg.__dict__)
+
