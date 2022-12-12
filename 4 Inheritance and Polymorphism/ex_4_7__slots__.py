ea6e8024c9b51810f041d66028b36790113c3e09
dv-markov <dmitry.v.markov@gmail.com>
2022-12-12 23:45:50 +0300

    WORK-COMMIT

--- 4 Inheritance and Polymorphism/ex_4_7__slots__.py
+++ 4 Inheritance and Polymorphism/ex_4_7__slots__.py
@@ -343,3 +343,63 @@ nota = notes[2]  # ссылка на ноту ми
 notes[3]._ton = -1 # изменение тональности ноты фа
 
 
+# Task 8
+class Function:
+    def __init__(self):
+        self._amplitude = 1.0     # амплитуда функции
+        self._bias = 0.0          # смещение функции по оси Oy
+
+    def __call__(self, x, *args, **kwargs):
+        return self._amplitude * self._get_function(x) + self._bias
+
+    def _get_function(self, x):
+        raise NotImplementedError('метод _get_function должен быть переопределен в дочернем классе')
+
+    @staticmethod
+    def __verify_numbers(num, tp):
+        if type(num) not in (int, float):
+            raise TypeError(f'{tp} должно быть числом')
+
+    def __add__(self, other):
+        self.__verify_numbers(other, 'смещение')
+        obj = self.__class__(self)
+        obj._bias = self._bias + other
+        return obj
+
+    def __mul__(self, other):
+        self.__verify_numbers(other, 'амплитуда')
+        obj = self.__class__(self)
+        obj._amplitude = self._amplitude * other
+        return obj
+
+
+class Linear(Function):
+    def __init__(self, *args):
+        super().__init__()
+        if len(args) == 2:
+            self._k, self._b = args
+        elif len(args) == 1 and isinstance(args[0], Linear):
+            self._k, self._b = args[0].get_kb()
+        else:
+            raise AttributeError('Неверный ввод аргументов при создании класса Liner')
+
+    def get_kb(self):
+        return self._k, self._b
+
+    def _get_function(self, x):
+        return self._k * x + self._b
+
+
+f = Linear(1, 0.5)
+f2 = f + 10   # изменение смещения (атрибут _bias)
+y1 = f(0)     # 0.5
+print(y1, f.__dict__)
+y2 = f2(0)    # 10.5
+print(y2, f2.__dict__)
+
+f = Linear(1, 0.5)
+f2 = f * 5    # изменение амплитуды (атрибут _amplitude)
+y1 = f(0)     # 0.5
+print(y1)
+y2 = f2(0)    # 2.5
+print(y2)
