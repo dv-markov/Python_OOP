e16677833d574db296f92b5c1a38a956a2b915e1
dv-markov <dmitry.v.markov@gmail.com>
2022-12-15 01:53:29 +0300

    WORK-COMMIT

--- 4 Inheritance and Polymorphism/ex4_8_Burden_Of_Legacy.py
+++ 4 Inheritance and Polymorphism/ex4_8_Burden_Of_Legacy.py
@@ -10,10 +10,12 @@ class Vertex:
 
 
 class Link:
-    def __init__(self, v1: Vertex, v2: Vertex):
+    def __init__(self, v1: Vertex, v2: Vertex, dist: int = 1):
         self._v1 = v1
         self._v2 = v2
-        self._dist = 1
+        self._dist = dist
+        self.set_vector_record(v1)
+        self.set_vector_record(v2)
 
     @property
     def v1(self):
@@ -31,17 +33,24 @@ class Link:
     def dist(self, value):
         self._dist = value
 
+    def set_vector_record(self, v):
+        if self not in v.links:
+            v.links.append(self)
+
+    def get_2nd_vertex(self, v):
+        return self.v2 if v is self.v1 else self.v1
+
 
 class LinkedGraph:
     def __init__(self):
         self._links = []
         self._vertex = []
 
-    def add_vertex(self, v):
+    def add_vertex(self, v: Vertex):
         if v not in self._vertex:
             self._vertex.append(v)
-            for l in v.links:
-                self.add_link(l)
+            # for l in v.links:
+            #     self.add_link(l)
 
     def add_link(self, link):
         if link not in self._links:
@@ -51,3 +60,34 @@ class LinkedGraph:
 
     def find_path(self, start_v, stop_v):
         # создать матрицу смежности
+        v_number = len(self._vertex)
+        adj_matrix = [[None for _ in range(v_number)] for _ in range(v_number)]
+        print(adj_matrix)
+        for i, v1 in enumerate(self._vertex):
+            # adj_matrix[i][i] = 0
+            # print(v1.links)
+            v1_edges = {edge.get_2nd_vertex(v1): edge.dist for edge in v1.links}
+            print(v1_edges)
+            # for j, v2 in enumerate(self._vertex[(i+1):]):
+            for j, v2 in enumerate(self._vertex):
+                adj_matrix[i][j] = v1_edges.get(v2, 0)
+        # adj_matrix[v_number-1][v_number-1] = 0
+        return adj_matrix
+
+# преобразование треугольной матрицы в симметричную
+# for i in range(num_rows):
+#     for j in range(i, num_cols):
+#         matrix[j][i] = matrix[i][j]
+
+
+lg = LinkedGraph()
+v_list = [Vertex() for _ in range(3)]
+link_list = [Link(v_list[0], v_list[1], 1), Link(v_list[1], v_list[2], 2), Link(v_list[0], v_list[2], 5)]
+for link in link_list:
+    print(f'{link}: {link.v1}, {link.v2}, {link.dist}')
+    lg.add_link(link)
+# print(lg._links)
+# print(lg._vertex)
+
+print(lg.find_path(v_list[0], v_list[2]))
+
