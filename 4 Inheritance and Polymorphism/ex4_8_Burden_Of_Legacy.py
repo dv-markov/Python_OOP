5a1b5bbae2b090ce7cbc8b25526739071bea08c9
dv-markov <dmitry.v.markov@gmail.com>
2022-12-15 00:05:20 +0300

    WORK-COMMIT

---
+++ 4 Inheritance and Polymorphism/ex4_8_Burden_Of_Legacy.py
@@ -0,0 +1,53 @@
+# 4.8 Испытание "Бремя наследия"
+
+class Vertex:
+    def __init__(self):
+        self._links = []
+
+    @property
+    def links(self):
+        return self._links
+
+
+class Link:
+    def __init__(self, v1: Vertex, v2: Vertex):
+        self._v1 = v1
+        self._v2 = v2
+        self._dist = 1
+
+    @property
+    def v1(self):
+        return self._v1
+
+    @property
+    def v2(self):
+        return self._v2
+
+    @property
+    def dist(self):
+        return self._dist
+
+    @dist.setter
+    def dist(self, value):
+        self._dist = value
+
+
+class LinkedGraph:
+    def __init__(self):
+        self._links = []
+        self._vertex = []
+
+    def add_vertex(self, v):
+        if v not in self._vertex:
+            self._vertex.append(v)
+            for l in v.links:
+                self.add_link(l)
+
+    def add_link(self, link):
+        if link not in self._links:
+            self._links.append(link)
+            self.add_vertex(link.v1)
+            self.add_vertex(link.v2)
+
+    def find_path(self, start_v, stop_v):
+        # создать матрицу смежности
