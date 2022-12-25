c75fe822d24421c827d377c33b892f137601fa65
dv-markov <dmitry.v.markov@gmail.com>
2022-12-25 22:24:01 +0300

    WORK-COMMIT

--- 5 Exceptions and content managers/ex5_2_finally_else.py
+++ 5 Exceptions and content managers/ex5_2_finally_else.py
@@ -282,40 +282,43 @@ for r in lst_not_collision:
 
 
 # тесты
-r = Rect(1, 2, 10, 20)
-assert r._x == 1 and r._y == 2 and r._width == 10 and r._height == 20, "неверные значения атрибутов объекта класса Rect"
-
-r2 = Rect(1.0, 2, 10.5, 20)
-
-try:
-    r2 = Rect(0, 2, 0, 20)
-except ValueError:
-    assert True
-else:
-    assert False, "не сгенерировалось исключение ValueError при создании объекта Rect(0, 2, 0, 20)"
-
-
-assert len(lst_rect) == 4, "список lst_rect содержит не 4 элемента"
-assert len(lst_not_collision) == 1, "неверное число элементов в списке lst_not_collision"
-
-def not_collision(rect):
-    for x in lst_rect:
-        try:
-            if x != rect:
-                rect.is_collision(x)
-        except TypeError:
-            return False
-    return True
-
-f = list(filter(not_collision, lst_rect))
-assert lst_not_collision == f, "неверно выделены не пересекающиеся прямоугольники, возможно, некорректно работает метод is_collision"
-
-r = Rect(3, 2, 2, 5)
-rr = Rect(1, 4, 6, 2)
-
-try:
-    r.is_collision(rr)
-except TypeError:
-    assert True
-else:
-    assert False, "не сгенерировалось исключение TypeError при вызове метода is_collision() для прямоугольников Rect(3, 2, 2, 5) и Rect(1, 4, 6, 2)"
+# r = Rect(1, 2, 10, 20)
+# assert r._x == 1 and r._y == 2 and r._width == 10 and r._height == 20, \
+#     "неверные значения атрибутов объекта класса Rect"
+#
+# r2 = Rect(1.0, 2, 10.5, 20)
+#
+# try:
+#     r2 = Rect(0, 2, 0, 20)
+# except ValueError:
+#     assert True
+# else:
+#     assert False, "не сгенерировалось исключение ValueError при создании объекта Rect(0, 2, 0, 20)"
+#
+#
+# assert len(lst_rect) == 4, "список lst_rect содержит не 4 элемента"
+# assert len(lst_not_collision) == 1, "неверное число элементов в списке lst_not_collision"
+#
+# def not_collision(rect):
+#     for x in lst_rect:
+#         try:
+#             if x != rect:
+#                 rect.is_collision(x)
+#         except TypeError:
+#             return False
+#     return True
+#
+# f = list(filter(not_collision, lst_rect))
+# assert lst_not_collision == f, "неверно выделены не пересекающиеся прямоугольники, " \
+#                                "возможно, некорректно работает метод is_collision"
+#
+# r = Rect(3, 2, 2, 5)
+# rr = Rect(1, 4, 6, 2)
+#
+# try:
+#     r.is_collision(rr)
+# except TypeError:
+#     assert True
+# else:
+#     assert False, "не сгенерировалось исключение TypeError при вызове метода is_collision() " \
+#                   "для прямоугольников Rect(3, 2, 2, 5) и Rect(1, 4, 6, 2)"
--- 5 Exceptions and content managers/ex5_3_propagation_exceptions.py
+++ 5 Exceptions and content managers/ex5_3_propagation_exceptions.py
@@ -1,2 +1,138 @@
 # 5.3 Распространение исключений (propagation exceptions)
 
+# стек вызова функций, отображающий распространение исключений
+# def func2():
+#     return 1 / 0
+#
+# def func1():
+#     return func2()
+#
+# print('Я к вам пишу - чего же боле?')
+# print('Что я могу еще сказать?')
+# print('Теперь, я знаю, в вашей воле')
+# func1()
+# print('Меня презреньем наказать.')
+# print('Но вы, к моей несчастной доле')
+# print('Хоть каплю жалости храня,')
+# print('Вы не оставите меня.')
+
+# обработка исключений в стеке вызова функций на разных уровнях
+# уровень main()
+def func2():
+    return 1 / 0
+
+
+def func1():
+    return func2()
+
+
+def poem_pt1():
+    print('Я к вам пишу - чего же боле?')
+    print('Что я могу еще сказать?')
+    print('Теперь, я знаю, в вашей воле')
+
+
+def poem_pt2():
+    print('Меня презреньем наказать.')
+    print('Но вы, к моей несчастной доле')
+    print('Хоть каплю жалости храня,')
+    print('Вы не оставите меня.')
+
+
+poem_pt1()
+try:
+    func1()
+except:
+    print("func1 error")
+poem_pt2()
+print()
+
+# На уровне func1
+def func2():
+    return 1 / 0
+
+
+def func1():
+    try:
+        return func2()
+    except:
+        print("func1 error")
+
+
+poem_pt1()
+func1()
+poem_pt2()
+print()
+
+
+# На уровне func2
+def func2():
+    try:
+        return 1 / 0
+    except:
+        print("func2 error")
+
+
+def func1():
+    try:
+        return func2()
+    except:
+        print("func1 error")
+
+
+poem_pt1()
+func1()
+poem_pt2()
+print()
+
+# как только исключение обрабатывается, дальше оно уже не всплывает
+# если обработано исключение func2 error, то func1 error уже не появится
+# можно обрабатывать исключения на разных уровнях стека вызова
+
+# обычно на уровне критических функций генерируются исключения,
+# а обрабатываются они уже на более глобальных уровнях
+# это называется распространение исключений (exception propagation)
+# и позволяет создавать гибкий и безопасный код
+
+
+# Task 3
+# Variant 0 - my
+# def input_int_numbers(line):
+#     lst = []
+#     for char in line.split():
+#         try:
+#             lst.append(int(char))
+#         except ValueError:
+#             raise TypeError('все числа должны быть целыми')
+#     return tuple(lst)
+#
+#
+# while True:
+#     s = input()
+#     try:
+#         res = input_int_numbers(s)
+#         print(*res)
+#         break
+#     except TypeError:
+#         continue
+
+# Variant 1 - my refactored
+# def input_int_numbers(line):
+#     try:
+#         return map(int, line.split())
+#     except ValueError:
+#         raise TypeError('все числа должны быть целыми')
+#
+#
+# while True:
+#     try:
+#         print(*input_int_numbers(input()))
+#         break
+#     except (ValueError, TypeError):
+#         continue
+
+
+# Task 4
+class ValidatorString:
+    def __init__(self, min_length, max_length, chars):
+        pass
