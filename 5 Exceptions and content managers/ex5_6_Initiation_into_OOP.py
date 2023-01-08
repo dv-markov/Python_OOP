16faec0ea0679bb15b98fa1a46b22d8f52f7bdf9
dv-markov <dmitry.v.markov@gmail.com>
2023-01-08 10:15:39 +0300

    WORK-COMMIT

--- 5 Exceptions and content managers/ex5_6_Initiation_into_OOP.py
+++ 5 Exceptions and content managers/ex5_6_Initiation_into_OOP.py
@@ -1,2 +1,30 @@
 # 5.6 Посвящение в объектно-ориентированное программирование
+# Морской бой
 
+
+FIELD_SIZE = 10
+
+
+class Ship:
+    def __init__(self, length: int, tp: int = 1, x=None, y=None):
+        self._x = x
+        self._y = y
+        self._length = length
+        self._tp = tp
+        self._is_move = True
+        self._cells = [1] * length
+
+    def __setattr__(self, key, value):
+        match key:
+            case '_x' | '_y':
+                # verify coords
+                if value is not None and (type(value) != int or not 0 <= value < FIELD_SIZE):
+                    raise ValueError(f'Координаты корабля должны быть '
+                                     f'целыми числами в диапазоне от 0 до {FIELD_SIZE-1}')
+            case '_length':
+                if type(value) != int or not 0 < value < 5:
+                    raise ValueError('Длина корабля должна быть целым числом в диапазоне от 1 до 4')
+
+
+sh = Ship(4)
+print(sh.__dict__)
